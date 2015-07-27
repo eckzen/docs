5c0c61d2d62c8ccdd84c6dda5ecb1fd6429afd4f
eckzen <eckzen@yahoo.com>
2015-07-28 00:35:26 +0800

    added browser sync for auto reloading of the browser

--- gulpfile_windows.js
+++ gulpfile_windows.js
@@ -1,12 +1,12 @@
 var  	gulp = require ('gulp')
-	uglify = require('gulp-uglify')
-	//sass = require('gulp-ruby-sass')
-	sass = require('gulp-sass')
-	plumber = require('gulp-plumber')
-	concat = require('gulp-concat')
-	imagemin = require('gulp-imagemin')
-	prefix = require('gulp-autoprefixer');
-
+		uglify = require('gulp-uglify')
+		sass = require('gulp-sass')
+		plumber = require('gulp-plumber')
+		concat = require('gulp-concat')
+		imagemin = require('gulp-imagemin')
+		browserSync = require('browser-sync').create()
+		prefix = require('gulp-autoprefixer');
+		
 // Minification of Javascript file
 // Uglifies 
 gulp.task('scripts', function(){
@@ -16,18 +16,17 @@ gulp.task('scripts', function(){
 	.pipe(gulp.dest('build/js'))
 	
 });
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
 
-// // Sass styling
-// gulp.task('styles', function() {
-//     return sass('./sass/', {
-// 	     style: 'expanded' 
-// 	 })
-//     		.pipe( plumber() )
-//     		.pipe(prefix('last 2 versions'))
-// 	    	.pipe(concat('style.css'))
-// 	        	.pipe(gulp.dest('./'))
-	       	
-// });
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("sass/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
 
 //Gulp SASS
 gulp.task('sass', function () {
@@ -37,6 +36,7 @@ gulp.task('sass', function () {
 	.pipe(prefix('last 2 versions'))
 	.pipe(concat('style.css'))
 	.pipe(gulp.dest('./'))
+	.pipe(browserSync.stream());
 });
 
 gulp.task('sass:watch', function () {
@@ -53,9 +53,10 @@ gulp.task('image', function(){
 // Watch task
 /*Looks for a change */
 gulp.task('watch', function(){ /*this is a build in watch method*/
+	// livereload.listen();
 	gulp.watch('js/*.js',  ['scripts']); /*watch the path and do scripts task*/ 
 	gulp.watch('sass/*.scss',  ['sass']);
 });
 
 
-gulp.task('default', ['scripts',  'sass', 'image', 'watch' ]);
+gulp.task('default', ['scripts',  'sass', 'image', 'watch', 'serve']);
